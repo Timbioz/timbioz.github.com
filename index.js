50c39ad68a326fdb831c984d50aaaaedf4d350b5
TIMBIOZ-PC\Timbioz <volkovtim@gmail.com>
2018-02-15 13:31:43 +0300

    l

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-    Cookies.set('timbioz', 'volkovtim@gmail.com_ambercond@gmail.com');
-    alert("Come On");
+    //Cookies.set('timbioz', 'volkovtim@gmail.com_ambercond@gmail.com');
+    //alert("Come On");
 });
 
 function alertFunc() {
